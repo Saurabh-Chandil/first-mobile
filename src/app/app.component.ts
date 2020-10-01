7f9f96d8d7016ebde51f2577984ca5bb6b6b1788
Saurabh-Chandil <schandil@cisco.com>
2020-10-01 20:57:06 +0530

    collapsible menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'first-mobile';
+  displayMenu = false;
+
+  ngOnInit(): void {
+    this.displayMenu = false;
+  }
+  
+  barsClicked() {
+    console.log("BARs clicked");
+    this.displayMenu = true;
+  }
+
+  hideMenu() {
+    console.log("hide menu");
+    this.displayMenu = false;
+  }
 }
